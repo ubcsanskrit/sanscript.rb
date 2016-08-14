e701fdb38dee6d520df6cd8dfe050aa56ee03ab6
Tim Bellefleur <nomoon@phoebus.ca>
2016-08-13 17:44:02 -0700

    makefile generation if rust exists.

--- rust/extconf.rb
+++ rust/extconf.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 if !system("cargo --version") || !system("rustc --version")
-  raise "You have to install Rust with Cargo (https://www.rust-lang.org/)"
+  create_makefile("sanscript")
+else
+  create_makefile("sanscript")
+  File.write("Makefile", "all:\n  cargo build --release\nclean:\n  rm -rf target\ninstall: ;")
 end
