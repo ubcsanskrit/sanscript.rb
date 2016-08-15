a5545459884dc714dad16af41c01501ed1591924
Tim Bellefleur <nomoon@phoebus.ca>
2016-08-15 14:42:14 -0700

    Better makefile/clean handling for development (rake compile does not clean).

--- rust/extconf.rb
+++ rust/extconf.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 require "mkmf"
-unless !system("cargo --version") || !system("rustc --version")
+if !system("cargo --version") || !system("rustc --version")
+  File.unlink("Makefile")
+else
   create_makefile("sanscript")
-  File.write("Makefile", "all:\n\tcargo build --release\n\nclean:\n\trm -rf target\n\ninstall:\n\tmv target/release/libsanscript.* .\n\trm -rf target\n")
+  File.write("Makefile", "all:\n\tcargo build --release\n\nclean:\n\trm -rf target\n\trm libsanscript.*\n\ninstall:\n\tmv target/release/libsanscript.* .\n\trm -rf target\n")
 end
