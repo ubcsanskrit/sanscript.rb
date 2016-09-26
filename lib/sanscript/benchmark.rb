0c6d8c8752444ed33c6553e1653a2844a43ab55b
Tim Bellefleur <nomoon@phoebus.ca>
2016-09-26 16:35:39 -0700

    Benchmark parameterization and integration with specs.

--- lib/sanscript/benchmark.rb
+++ lib/sanscript/benchmark.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-#:nocov:
 
+# :nocov:
 begin
   require "benchmark/ips"
 rescue LoadError
@@ -10,6 +10,7 @@ rescue LoadError
     end
   end
 end
+# :nocov:
 
 module Sanscript
   # Benchmark/testing module.
@@ -17,7 +18,7 @@ module Sanscript
     module_function
 
     # Runs benchmark-ips test on detection methods.
-    def detect!
+    def detect!(time = 2, warmup = 1)
       deva_string = "नानाशास्त्रसुभाषितामृतरसैः श्रोत्रोत्सवं कुर्वतां येषां यान्ति दिनानि पण्डितजनव्यायामखिन्नात्मनाम् तेषां जन्म च जीवितं च सुकृतं तैर् एव भूर् भूषिता शेषैह् किं पशुवद् विवेकरहितैर् भूभारभूतैर् नरः"
       malayalam_string = "നാനാശാസ്ത്രസുഭാഷിതാമൃതരസൈഃ ശ്രോത്രോത്സവം കുര്വതാം യേഷാം യാന്തി ദിനാനി പണ്ഡിതജനവ്യായാമഖിന്നാത്മനാമ് തേഷാം ജന്മ ച ജീവിതം ച സുകൃതം തൈര് ഏവ ഭൂര് ഭൂഷിതാ ശേഷൈഹ് കിം പശുവദ് വിവേകരഹിതൈര് ഭൂഭാരഭൂതൈര് നരഃ"
       iast_string = "nānāśāstrasubhāṣitāmṛtarasaiḥ śrotrotsavaṃ kurvatāṃ yeṣāṃ yānti dināni paṇḍitajanavyāyāmakhinnātmanām teṣāṃ janma ca jīvitaṃ ca sukṛtaṃ tair eva bhūr bhūṣitā śeṣaih kiṃ paśuvad vivekarahitair bhūbhārabhūtair naraḥ"
@@ -25,21 +26,21 @@ module Sanscript
       hk_string = "nAnAzAstrasubhASitAmRtarasaiH zrotrotsavaM kurvatAM yeSAM yAnti dinAni paNDitajanavyAyAmakhinnAtmanAm teSAM janma ca jIvitaM ca sukRtaM tair eva bhUr bhUSitA zeSaih kiM pazuvad vivekarahitair bhUbhArabhUtair naraH"
 
       ::Benchmark.ips do |x|
-        x.config(time: 5, warmup: 1)
+        x.config(time: time, warmup: warmup)
         x.report("Detect Devanagari") do
-          raise unless Sanscript::Detect.detect_scheme(deva_string) == :devanagari
+          Sanscript::Detect.detect_scheme(deva_string)
         end
         x.report("Detect Malayalam") do
-          raise unless Sanscript::Detect.detect_scheme(malayalam_string) == :malayalam
+          Sanscript::Detect.detect_scheme(malayalam_string)
         end
         x.report("Detect IAST") do
-          raise unless Sanscript::Detect.detect_scheme(iast_string) == :iast
+          Sanscript::Detect.detect_scheme(iast_string)
         end
         x.report("Detect SLP1") do
-          raise unless Sanscript::Detect.detect_scheme(slp1_string) == :slp1
+          Sanscript::Detect.detect_scheme(slp1_string)
         end
         x.report("Detect HK") do
-          raise unless Sanscript::Detect.detect_scheme(hk_string) == :hk
+          Sanscript::Detect.detect_scheme(hk_string)
         end
         x.compare!
       end
@@ -47,13 +48,13 @@ module Sanscript
     end
 
     # Runs benchmark-ips test on roman-source transliteration methods.
-    def transliterate_roman!
+    def transliterate_roman!(time = 2, warmup = 1)
       iast_string = "nānāśāstrasubhāṣitāmṛtarasaiḥ śrotrotsavaṃ kurvatāṃ yeṣāṃ yānti dināni paṇḍitajanavyāyāmakhinnātmanām teṣāṃ janma ca jīvitaṃ ca sukṛtaṃ tair eva bhūr bhūṣitā śeṣaih kiṃ paśuvad vivekarahitair bhūbhārabhūtair naraḥ"
       slp1_string = "nAnASAstrasuBAzitAmftarasEH SrotrotsavaM kurvatAM yezAM yAnti dinAni paRqitajanavyAyAmaKinnAtmanAm tezAM janma ca jIvitaM ca sukftaM tEr eva BUr BUzitA SezEh kiM paSuvad vivekarahitEr BUBAraBUtEr naraH"
       hk_string = "nAnAzAstrasubhASitAmRtarasaiH zrotrotsavaM kurvatAM yeSAM yAnti dinAni paNDitajanavyAyAmakhinnAtmanAm teSAM janma ca jIvitaM ca sukRtaM tair eva bhUr bhUSitA zeSaih kiM pazuvad vivekarahitair bhUbhArabhUtair naraH"
 
       ::Benchmark.ips do |x|
-        x.config(time: 3, warmup: 2)
+        x.config(time: time, warmup: warmup)
 
         x.report("IAST==>Devanagari") do
           Sanscript.transliterate(iast_string, :iast, :devanagari)
@@ -88,11 +89,11 @@ module Sanscript
     end
 
     # Runs benchmark-ips test on brahmic-source transliteration methods.
-    def transliterate_brahmic!
+    def transliterate_brahmic!(time = 2, warmup = 1)
       deva_string = "नानाशास्त्रसुभाषितामृतरसैः श्रोत्रोत्सवं कुर्वतां येषां यान्ति दिनानि पण्डितजनव्यायामखिन्नात्मनाम् तेषां जन्म च जीवितं च सुकृतं तैर् एव भूर् भूषिता शेषैह् किं पशुवद् विवेकरहितैर् भूभारभूतैर् नरः"
 
       ::Benchmark.ips do |x|
-        x.config(time: 5, warmup: 2)
+        x.config(time: time, warmup: warmup)
         x.report("Devanagari==>IAST") do
           Sanscript.transliterate(deva_string, :devanagari, :iast)
         end
