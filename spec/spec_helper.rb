9fa256c92a286ceeca0be66d1ad092de02b56560
Tim Bellefleur <nomoon@phoebus.ca>
2017-01-06 06:25:11 -0800

    Update Travis CI targets, rake gem, and switch to coveralls for code coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
-require "codeclimate-test-reporter"
-SimpleCov.start do
-  add_filter "vendor"
-  add_filter "spec"
-  formatter SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::HTMLFormatter,
-      CodeClimate::TestReporter::Formatter,
-    ]
-  )
-end
+require "coveralls"
+Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "sanscript"
